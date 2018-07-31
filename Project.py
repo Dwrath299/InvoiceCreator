e6552c142f0eb83463e26e979a076e50eea805c1
Dallin <Dwrath299@gmail.com>
2018-07-31 11:14:07 -0600

    Create Project class
    Project class will control each project in the gui but also use the calculate functions to get the dollar amounts.

---
+++ Project.py
@@ -0,0 +1,9 @@
+class Project:
+  def __init__():
+    self.hours = 0
+    self.minutes = 0
+    self.dollars = 0
+    #Need to create the GUI objects for the class
+  # Get the dollar amount for the time put in.
+  def calculate_dollars:
+    self.dollars = Calculate.convert_time_to_dollars(self.hours, self.minutes)
